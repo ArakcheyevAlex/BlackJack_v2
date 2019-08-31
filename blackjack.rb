92ee38c629c952740b24605b9c27ffb921c6b815
Alex <arakcheyev.alex@gmail.com>
2019-08-31 15:46:31 +0700

    Fix game runner method name

--- blackjack.rb
+++ blackjack.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
 class BlackJack
-  def run
-  end
+  def run; end
 end
-
-
--- main.rb
+++ main.rb
@@ -1,1 +1,5 @@
-main.rb
+# frozen_string_literal: true
+
+require './blackjack'
+
+BlackJack.new.run
